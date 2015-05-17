152483dceade552c478cf7c879a5b13d9e1c0162
natiziv <natiiziv@gmail.com>
2015-05-17 13:55:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-STC_PATH="/Users/welefen/Develop/svn/STC/src"
+STC_PATH="/Users/natiz/Develop/svn/STC/src"
+PHP_PATH="/usr/bin/php"
 path=`dirname $0`;
 first=${path:0:1};
 if [[ $first != '/' ]];then
@@ -14,7 +15,7 @@ if [ ! -f ${path}"/config.php" ];then
 	cp $STC_PATH/config/config.php ${path};
 fi
 #path=$(pwd);
-/usr/local/bin/php $STC_PATH/index.php ${path} test $1;
+$PHP_PATH $STC_PATH/index.php ${path} test $1;
 if [ -f ${path}"/stc.error.log" ]; then
     rm -rf ${path}"/stc.error.log";
     #exit 1;
